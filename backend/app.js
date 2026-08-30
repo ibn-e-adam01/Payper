809c54fc48b36099bf9db4de06aaf6bb90ad0673
user.name: "Asrar Firdoos <user.email: "theasrarfirdous@gmail.com>
2026-08-30 16:47:13 +0530

    feat: modify CORS origin

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_LIVE_URL],
+    origin: process.env.FRONTEND_LIVE_URL,
     allowedHeaders: ['Content-Type'],
     methods: ['POST', 'GET', 'PATCH', 'PUT', 'DELETE'],
     credentials: true
