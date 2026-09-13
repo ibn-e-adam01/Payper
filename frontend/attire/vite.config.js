2cbcc51013c3921a986223763d80c8245f0f0bac
user.name: "Asrar Firdoos <user.email: "theasrarfirdous@gmail.com>
2026-09-13 15:02:53 +0530

    feat: PWA configuration for App Installing on Mobile

--- frontend/attire/vite.config.js
+++ frontend/attire/vite.config.js
@@ -1,13 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest:{
+    "name": "Attire",
+    "short_name": "Attire",
+    "description": "Attire - Professional Marketplace",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#000000",
+    "icons":[
+    {   "src": "/icons/iconAttire.png",
+        "sizes": "192x192",
+        "type": "image/png"
+    },
+    ],
+    },
+    })
+  ],
   server: {
 
     allowedHosts: ['tubular-playpen-unknown.ngrok-free.dev']
     
   }
-})
+});
